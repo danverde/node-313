cc0dc646c2ba421968b362231bb8624f0df416bd
Danverde <gre12014@byui.edu>
2018-06-30 12:54:25 -0600

    initial routs working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 
+/* setup express */
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,8 +17,10 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
 app.get('/login', (req, res) => res.render('pages/login'));
 app.get('/register', (req, res) => res.render('pages/register'));
-// app.get('/viewbuild', (req, res) => res.redirect('pages/viewBuild/1')); // TODO needs an OPTIONAL ID parameter b& logic
-app.get('/viewbuild/:buildId', (req, res) => res.render('pages/viewBuild')); // TODO needs an OPTIONAL ID parameter b& logic
+app.get('/viewbuild', sendToActiveBuild);
+// TODO this will need logic to fill out the JSON
+app.get('/viewbuild/:buildId', (req, res) => res.render('pages/viewBuild'));
+app.get('/items', (req, res) => res.redirect('items/1'));
 app.get('/items/:typeId', (req, res) => res.render('pages/items'));
 
 /* all POST requests */
@@ -35,7 +38,7 @@ app.delete('/items/:typeId/:itemId', deleteItem);
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 
 
-/* helper function */
+/* helper functions */
 
 function login(req, res) {
 
@@ -43,6 +46,11 @@ function login(req, res) {
 
 function register(req, res) {
 
+}
+
+function sendToActiveBuild(req, res) {
+    var activeBuildId = 1;
+    res.redirect(`viewBuild/${activeBuildId}`);
 }
 
 function setActiveBuild(req, res) {
