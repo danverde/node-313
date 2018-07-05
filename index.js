05943b18357e0698046f66c1661be7c026f7e9d9
danverde <gre12014@byui.edu>
2018-07-05 12:36:24 -0600

    added base HTML

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
  
 
 /* All GET requests */
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => res.render('pages/home'));
 app.get('/login', (req, res) => res.render('pages/login'));
 app.get('/register', (req, res) => res.render('pages/register'));
 app.get('/viewbuild', sendToActiveBuild);
