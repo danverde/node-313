bd3cb65b35e3cd1b9494885253571d6f9a5a1636
danverde <gre12014@byui.edu>
2018-07-05 16:01:48 -0600

    added basic loggedin info

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const connectionString = process.env.DATABASE_URL; // TODO get this working! || 
 const pool = new Pool({connectionString: connectionString});
 
 
+/* global server vars */
+var loggedIn = true;
+
+
 /* setup express */
 var app = express();
 
@@ -18,19 +22,81 @@ app.set('view engine', 'ejs');
  
 
 /* All GET requests */
-app.get('/', (req, res) => res.render('pages/home', {loggedIn: true}));
-app.get('/login', (req, res) => res.render('pages/login'));
-app.get('/register', (req, res) => res.render('pages/register'));
-app.get('/viewbuild', sendToActiveBuild);
-// TODO this will need logic to fill out the JSON
-app.get('/viewbuild/:buildId', (req, res) => res.render('pages/viewBuild'));
-app.get('/items', (req, res) => res.redirect('items/1'));
-app.get('/items/:typeId', (req, res) => res.render('pages/items'));
+app.get('/', (req, res) => {
+    if (loggedIn === false) {
+        res.redirect('login');
+        return;
+    }
+
+    res.render('pages/home', {loggedIn});
+});
+
+app.get('/logout', (req, res) => {
+    if (loggedIn === true) {
+        loggedIn = false;
+    } 
+    res.redirect('login');
+});
+
+app.get('/login', (req, res) => {
+    /* shouldn't be able to log in if you're lready logged in... */
+    if (loggedIn === true)
+        res.redirect('/');
+    else
+        res.render('pages/login', {loggedIn});
+});
+
+app.get('/register', (req, res) => {
+    if (loggedIn === true) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('pages/register', {loggedIn});
+});
+
+app.get('/builds', (req, res) => {
+    if (loggedIn === false) {
+        res.redirect('login');
+        return;
+    }
+
+    var activeBuildId = 1;
+    res.redirect(`builds/${activeBuildId}`);
+});
+
+app.get('/builds/:buildId', (req, res) => {
+    if (loggedIn === false) {
+        res.redirect('login');
+        return;
+    }
+
+    res.render('pages/builds', {loggedIn});
+});
+
+app.get('/items', (req, res) => {
+    if (loggedIn === false) {
+        res.redirect('login');
+        return;
+    }
+
+    res.redirect('items/1');
+});
+
+app.get('/items/:typeId', (req, res) => {
+    if (loggedIn === false) {
+        res.redirect('login');
+        return;
+    }
+
+    res.render('pages/items', {loggedIn});
+});
+
 
 /* all POST requests */
 app.post('/login', login);
 app.post('/register', register);
-app.post('/viewbuild/:buildId', setActiveBuild);
+app.post('/builds/:buildId', setActiveBuild);
 app.post('/items/:typeId/:itemId', addItemToBuild);
 
 /* all PUT requests */
@@ -52,10 +118,6 @@ function register(req, res) {
 
 }
 
-function sendToActiveBuild(req, res) {
-    var activeBuildId = 1;
-    res.redirect(`viewBuild/${activeBuildId}`);
-}
 
 function setActiveBuild(req, res) {
 
